90f66ede0c5fe6f99da7dc0813905109e3d08661
Francesco-Maisto <franco.maisto@outlook.com>
2024-05-07 19:18:54 +0200

    prova

---
+++ src/main/java/it/maisto/GestoneImmobili/GestoneImmobiliApplication.java
@@ -0,0 +1,13 @@
+package it.maisto.GestoneImmobili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestoneImmobiliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestoneImmobiliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/maisto/GestoneImmobili/GestoneImmobiliApplicationTests.java
@@ -0,0 +1,13 @@
+package it.maisto.GestoneImmobili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestoneImmobiliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
